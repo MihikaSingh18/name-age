538f1f93b93c61d9e1c1b67a18f1a39606e354ee
MihikaSingh18 <138354730+MihikaSingh18@users.noreply.github.com>
2023-07-15 21:18:43 +0530

    Add files via upload

---
+++ age and name.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      singh
+#
+# Created:     15-07-2023
+# Copyright:   (c) singh 2023
+# Licence:     <your licence>
+#--------------------------------------------------------------------------
+
+
+old_age=input("enter your age : ")
+new_age=int(old_age) + 2
+print(new_age)
+
+your_name=input("enter your name :  ")
+new_name=str(your_name) + " " + "is a good girl"
+print(new_name)
+
+
+first_number=input("enter first number :  ")
+second_number=input("enter second number :  ")
+sum=float(first_number) + float(second_number)
+print("sum is : " + str(sum))
